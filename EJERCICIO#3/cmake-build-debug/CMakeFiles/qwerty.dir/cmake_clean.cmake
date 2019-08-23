56949e395b4d93bcff3d66b691db64fbdf5009fd
dg17001-prn335-2019 <dg17001@ues.edu.sv>
2019-08-22 18:01:47 -0600

    ejercicio #3

---
+++ EJERCICIO#3/cmake-build-debug/CMakeFiles/qwerty.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qwerty.dir/main.c.o"
+  "qwerty.pdb"
+  "qwerty"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/qwerty.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ EJERCICIO#3/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+int main() {
+    int n, i = 0;
+
+    printf("Ingrese el tamanio de la lista ");
+    scanf("%d", &n);
+
+    int lista[n];
+
+    for (i = 0; i < n; i++) {
+        printf("Ingrese el valor de la lista en la posicion %d ", i + 1);
+        scanf("%d", &lista[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("En posicion %d el valor es %d\n", i, lista[i]);
+    }
+
+    printf("\n\n");
+
+    float suma =0;
+    for(i=0; i<n; i++){
+        suma+=lista[i];
+    }
+    float media = suma / n;
+    printf("La media es %.2f", media);
+    printf("\n");
+
+    float diferencia, cuadrado, total, varianza, devest=0;
+
+    for (i=0; i<n; i++){
+        diferencia = lista[i]-media;
+        cuadrado = diferencia * diferencia;
+        total+=cuadrado;
+    }
+    varianza = total/n;
+    printf("La varianza es: %.4f", varianza);
+    printf("\n");
+
+    devest = sqrt(varianza);
+    printf("La desviacion estándar es: %.4f", devest);
+}
